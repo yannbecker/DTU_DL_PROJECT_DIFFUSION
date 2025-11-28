62df6dee676d9cf5cccbbb0e3bda10b973d025e2
KOLAR Pierre-Eduard <kolarpierreeduard@gmail.com>
2025-11-28 15:27:10 +0100

    Adjusted parser for diffusion train

---
+++ src/diffusion/diffusion_train.py
@@ -0,0 +1,99 @@
+"""
+Train a diffusion model on images.
+"""
+
+import argparse
+
+from utils import dist_util, logger
+from preprocessing.cell_datasets_loader import load_data
+from utils.resample import create_named_schedule_sampler
+from utils.script_util import (
+    model_and_diffusion_defaults,
+    create_model_and_diffusion,
+    args_to_dict,
+    add_dict_to_argparser,
+)
+from utils.train_util import TrainLoop
+
+import torch
+import numpy as np
+import random
+
+def main():
+    setup_seed(1234)
+    args = create_argparser().parse_args()
+
+    dist_util.setup_dist()
+    logger.configure(dir='../output/logs/'+args.model_name)  # log file
+
+    logger.log("creating model and diffusion...")
+    model, diffusion = create_model_and_diffusion(
+        **args_to_dict(args, model_and_diffusion_defaults().keys())
+    )
+    model.to(dist_util.dev())
+    schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
+
+    logger.log("creating data loader...")
+    data = load_data(
+        data_dir=args.data_dir,
+        batch_size=args.batch_size,
+        vae_path=args.vae_path,
+        train_vae=False,
+    )
+
+    logger.log("training...")
+    TrainLoop(
+        model=model,
+        diffusion=diffusion,
+        data=data,
+        batch_size=args.batch_size,
+        microbatch=args.microbatch,
+        lr=args.lr,
+        ema_rate=args.ema_rate,
+        log_interval=args.log_interval,
+        save_interval=args.save_interval,
+        resume_checkpoint=args.resume_checkpoint,
+        use_fp16=args.use_fp16,
+        fp16_scale_growth=args.fp16_scale_growth,
+        schedule_sampler=schedule_sampler,
+        weight_decay=args.weight_decay,
+        lr_anneal_steps=args.lr_anneal_steps,
+        model_name=args.model_name,
+        save_dir=args.save_dir
+    ).run_loop()
+
+
+def create_argparser():
+    defaults = dict(
+        data_dir="/work3/s193518/scIsoPred/data/bulk_processed_transcripts.h5ad",
+        schedule_sampler="uniform",
+        lr=1e-4,
+        weight_decay=0.0001,
+        lr_anneal_steps=5000,
+        batch_size=128,
+        microbatch=-1,  # -1 disables microbatches
+        ema_rate="0.9999",  # comma-separated list of EMA values
+        log_interval=10,
+        save_interval=1000,
+        resume_checkpoint="",
+        use_fp16=False,
+        fp16_scale_growth=1e-3,
+        vae_path = '/zhome/70/a/224464/DL_project17/DTU_DL_PROJECT_DIFFUSION/src/VAE/output/ae_checkpoint/vae_bulk_transcript_pca/model_seed=0_step=1999.pt',
+        model_name="bulk_diffusion",
+        save_dir='output/diffusion_checkpoint'
+    )
+    defaults.update(model_and_diffusion_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+    return parser
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+if __name__ == "__main__":
+    main()
