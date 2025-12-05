26b7fe1e8a44c8284a7e34e01acb0b0a0135d767
Thibaut Heim <thibaut.heim@etu.ec-lyon.fr>
2025-12-05 01:25:57 +0100

    Compute_random_forest

--- src/metrics/evaluations2.py
+++ src/metrics/evaluations2.py
@@ -7,9 +7,12 @@ import torch
 from torch import nn
 import numpy as np
 import anndata as ad
+import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import  train_test_split
 from sklearn.metrics import *
+import scanpy as sc
 
 
 # ==========
@@ -120,15 +123,68 @@ def compute_correlations():
     pass
 
 def compute_random_forest(
-        original, 
-        generated, 
+        
+        original : np.ndarray, 
+        generated : np.ndarray, 
         n_estimators = 1000, 
         max_depth= 5,      
         oob_score=True,
         class_weight = "balanced",
         random_state=1):
     
-    pass
+    """Computes Random Forest classification metrics between original and generated data.
+     Returns a dictionary with training accuracy, validation accuracy, AUC, OOB score, FPR and TPR and plots the ROC curve."""
+    full_data = np.concatenate((original, generated),axis=0)
+    full_data = ad.AnnData(full_data, dtype=np.float32)
+    adata = full_data
+    full_data.obs_names = [f"true_Cell" for i in range(original.shape[0])]+[f"gen_Cell" for i in range(generated.shape[0])]
+    sc.tl.pca(adata, svd_solver='arpack') # svd_solver 
+    real = adata[adata.obs_names=='true_Cell'].obsm['X_pca']
+    sim = adata[adata.obs_names=='gen_Cell'].obsm['X_pca']
+
+    data = np.concatenate((real,sim),axis=0)
+    label = np.concatenate((np.ones((real.shape[0])),np.zeros((sim.shape[0]))))
+
+    X_train,X_val,y_train,y_val = train_test_split(data, label,
+    test_size = 0.25,random_state = 1)
+
+    rfc1 = RandomForestClassifier(n_estimators = n_estimators, 
+                              max_depth= max_depth,      
+                              oob_score= oob_score,
+                              class_weight = class_weight,
+                              random_state=random_state)
+    rfc1.fit(X_train,y_train)
+    ## accuracy
+    rfc1_lab = rfc1.predict(X_train)
+    rfc1_pre = rfc1.predict(X_val)
+    print("OOB score of random forest:",rfc1.oob_score_)
+    train_acc = accuracy_score(y_train,rfc1_lab)
+    val_acc = accuracy_score(y_val,rfc1_pre)
+    print("accuracy in training set:",train_acc)
+    print("accuracy in validation set:",val_acc)
+    pre_y = rfc1.predict_proba(X_val)[:, 1]
+    fpr_Nb, tpr_Nb, _ = roc_curve(y_val, pre_y)
+    aucval = auc(fpr_Nb, tpr_Nb)   #Area Under Curve 
+    plt.figure(figsize=(10,8))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr_Nb, tpr_Nb,"r",linewidth = 3)
+    plt.grid()
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.title("ROC curve of random forest")
+    plt.text(0.15,0.9,"AUC = "+str(round(aucval,4)))
+    plt.show()
+    return {"train accuracy": train_acc, 
+            "validation accuracy": val_acc, 
+            "auc": aucval, 
+            "oob_score": rfc1.oob_score_, 
+            "False Positive Rate": fpr_Nb, 
+            "True Positive Rate": tpr_Nb}
+
+
+
 
 # ===========================
 #   Convenience objects
